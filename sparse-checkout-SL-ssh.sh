6194569a723b4db67eb2f2eefd745901dc96da5a
nckw <n.c.k.wardle@googlemail.com>
2017-03-22 08:15:39 +0000

    Update sparse-checkout-SL-ssh.sh

--- sparse-checkout-SL-ssh.sh
+++ sparse-checkout-SL-ssh.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir stats-tools; cd stats-tools
 git init
-git remote add origin git@github.com:nucleosynthesis/work-tools.git
+git remote add origin https://github.com/nucleosynthesis/work-tools
 git config core.sparsecheckout true; 
  echo stats-tools/simplifiedLikelihood.C >> .git/info/sparse-checkout
  echo stats-tools/makeLHInputs.py >> .git/info/sparse-checkout
