20f1c7a32d426260dd6c3042525957633e1e5d82
nucleosynthesis <n.c.k.wardle@googlemail.com>
2020-12-08 13:09:10 +0100

    Add a 8/4 bin version

---
+++ hinv-tools/vbfwstools/allTFSys_8bins.sh
@@ -0,0 +1,39 @@
+#!/bin/bash 
+python plotTF.py MTR_2017 8 1 2.4   "41.4 fb^{-1} (13 TeV, 2017)" "MTR"  "WJETS_WMUNU" "WJETS_SR" 
+python plotTF.py MTR_2017 8 0.2 2.0 "41.4 fb^{-1} (13 TeV, 2017)" "MTR"  "WJETS_WENU"  "WJETS_SR" 
+python plotTF.py MTR_2017 8 1.4 5.0   "41.5 fb^{-1} (13 TeV, 2017)" "MTR"  "EWKW_WMUNU" "EWKW_SR" 
+python plotTF.py MTR_2017 8 0 3.0 "41.4 fb^{-1} (13 TeV, 2017)" "MTR"  "EWKW_WENU"  "EWKW_SR" 
+python plotTF.py MTR_2017 8 0 0.2    "41.4 fb^{-1} (13 TeV, 2017)" "MTR"  "DY_ZMUMU"     "ZJETS_SR" 
+python plotTF.py MTR_2017 8 0 0.2    "41.4 fb^{-1} (13 TeV, 2017)" "MTR"  "DY_ZEE"       "ZJETS_SR" 
+python plotTF.py MTR_2017 8 0 0.2   "41.4 fb^{-1} (13 TeV, 2017)" "MTR"  "EWKZll_ZMUMU" "EWKZNUNU_SR" 
+python plotTF.py MTR_2017 8 0 0.2    "41.4 fb^{-1} (13 TeV, 2017)" "MTR"  "EWKZll_ZEE"   "EWKZNUNU_SR" 
+
+python plotTF.py VTR_2017 4 0 2.0   "41.5 fb^{-1} (13 TeV, 2017)" "VTR"  "WJETS_WMUNU" "WJETS_SR" 
+python plotTF.py VTR_2017 4 0 2.0 "41.5 fb^{-1} (13 TeV, 2017)" "VTR"  "WJETS_WENU"  "WJETS_SR" 
+python plotTF.py VTR_2017 4 0.5 4.0   "41.5 fb^{-1} (13 TeV, 2017)" "VTR"  "EWKW_WMUNU" "EWKW_SR" 
+python plotTF.py VTR_2017 4 0 3.0 "41.5 fb^{-1} (13 TeV, 2017)" "VTR"  "EWKW_WENU"  "EWKW_SR" 
+python plotTF.py VTR_2017 4 0 0.2    "41.5 fb^{-1} (13 TeV, 2017)" "VTR"  "DY_ZMUMU"     "ZJETS_SR" 
+python plotTF.py VTR_2017 4 0 0.2    "41.5 fb^{-1} (13 TeV, 2017)" "VTR"  "DY_ZEE"       "ZJETS_SR" 
+python plotTF.py VTR_2017 4 0 0.2   "41.5 fb^{-1} (13 TeV, 2017)" "VTR"  "EWKZll_ZMUMU" "EWKZNUNU_SR" 
+python plotTF.py VTR_2017 4 0 0.2    "41.5 fb^{-1} (13 TeV, 2017)" "VTR"  "EWKZll_ZEE"   "EWKZNUNU_SR" 
+
+
+python plotTF.py MTR_2018 8 1 2.4   "49.8 fb^{-1} (13 TeV, 2018)" "MTR"  "WJETS_WMUNU" "WJETS_SR" 
+python plotTF.py MTR_2018 8 0.2 2.0 "49.8 fb^{-1} (13 TeV, 2018)" "MTR"  "WJETS_WENU"  "WJETS_SR" 
+python plotTF.py MTR_2018 8 1.4 5.0   "59.8 fb^{-1} (13 TeV, 2018)" "MTR"  "EWKW_WMUNU" "EWKW_SR" 
+python plotTF.py MTR_2018 8 0 3.0   "49.8 fb^{-1} (13 TeV, 2018)" "MTR"  "EWKW_WENU"  "EWKW_SR" 
+python plotTF.py MTR_2018 8 0 0.2    "49.8 fb^{-1} (13 TeV, 2018)" "MTR"  "DY_ZMUMU"     "ZJETS_SR" 
+python plotTF.py MTR_2018 8 0 0.2    "49.8 fb^{-1} (13 TeV, 2018)" "MTR"  "DY_ZEE"       "ZJETS_SR" 
+python plotTF.py MTR_2018 8 0 0.2   "49.8 fb^{-1} (13 TeV, 2018)" "MTR"  "EWKZll_ZMUMU" "EWKZNUNU_SR" 
+python plotTF.py MTR_2018 8 0 0.2    "49.8 fb^{-1} (13 TeV, 2018)" "MTR"  "EWKZll_ZEE"   "EWKZNUNU_SR" 
+
+
+python plotTF.py VTR_2018 4 0 1.6   "58.8 fb^{-1} (13 TeV, 2018)" "VTR"  "WJETS_WMUNU" "WJETS_SR" 
+python plotTF.py VTR_2018 4 0 1.6 "58.8 fb^{-1} (13 TeV, 2018)" "VTR"  "WJETS_WENU"  "WJETS_SR" 
+python plotTF.py VTR_2018 4 1.5 5.0   "58.8 fb^{-1} (13 TeV, 2018)" "VTR"  "EWKW_WMUNU" "EWKW_SR" 
+python plotTF.py VTR_2018 4 0 3.0 "58.8 fb^{-1} (13 TeV, 2018)" "VTR"  "EWKW_WENU"  "EWKW_SR" 
+python plotTF.py VTR_2018 4 0 0.2    "58.8 fb^{-1} (13 TeV, 2018)" "VTR"  "DY_ZMUMU"     "ZJETS_SR" 
+python plotTF.py VTR_2018 4 0 0.2    "58.8 fb^{-1} (13 TeV, 2018)" "VTR"  "DY_ZEE"       "ZJETS_SR" 
+python plotTF.py VTR_2018 4 0 0.2   "58.8 fb^{-1} (13 TeV, 2018)" "VTR"  "EWKZll_ZMUMU" "EWKZNUNU_SR" 
+python plotTF.py VTR_2018 4 0 0.2    "58.8 fb^{-1} (13 TeV, 2018)" "VTR"  "EWKZll_ZEE"   "EWKZNUNU_SR" 
+
