5e3172a33f3676e175b2b527b75400e76566c402
nucleosynthesis <n.c.k.wardle@googlemail.com>
2021-04-09 18:21:42 +0200

    Super useful thing

---
+++ getFromCrab.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+BASE=${PWD}
+BFOLDER=$2
+cd $BASE
+#source /cvmfs/cms.cern.ch/crab3/crab.sh; 
+#eval `scramv1 runtime -sh`
+
+TIME=$(crab status $1 | grep "Task name" | cut -d ':' -f 2 | xargs)
+
+FOLDER=$(crab status $1 | grep "Task name" | cut -d ':' -f 3 | xargs)
+prefix="nckw_crab_"
+FOLDER=${FOLDER#"$prefix"}
+ext=""
+
+LOC="/store/user/nckw/${BFOLDER}/${FOLDER}/${TIME}/0000/"
+
+#echo $(xrdfs root://gfe02.grid.hep.ph.ic.ac.uk:1097/store/ ls ${LOC})
+#echo ${LOC}
+#exit 
+RANDOMFILE=out_$RANDOM
+
+mkdir -p output_crab/${BFOLDER}/$1
+cd /tmp/nckw/
+mkdir $RANDOMFILE
+cd $RANDOMFILE
+for FILE in $(xrdfs root://gfe02.grid.hep.ph.ic.ac.uk:1097/store/ ls ${LOC} | grep combine_output_); do
+  xrdcp root://gfe02.grid.hep.ph.ic.ac.uk:1097/${FILE} .
+done
+for FILE in $(ls combine_out*.tar); do tar -xvf ${FILE}; done 
+hadd -f output_${FOLDER}.root higgsCombine${ext}*.root 
+mv output_${FOLDER}.root $BASE/output_crab/${BFOLDER}/$1
+rm -f higgsCombine* *.tar
+cd /tmp/nckw
+rmdir $RANDOMFILE
+exit 
+
+
