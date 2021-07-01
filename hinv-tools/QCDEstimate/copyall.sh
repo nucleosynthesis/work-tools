849ceed7f8e32375ce7e33f3d446ab51dbc679b3
Samuel Webb <samuel.webb@cern.ch>
2021-07-01 11:43:42 +0100

    add copyall script

---
+++ hinv-tools/QCDEstimate/copyall.sh
@@ -0,0 +1,16 @@
+cp -p inputs/out_MTR_2017.root_qcdDD.root ~/invisible/QCD/hig-20-003/MTR_2017/.
+cp -p inputs/out_VTR_2017.root_qcdDD.root ~/invisible/QCD/hig-20-003/VTR_2017/. 
+cp -p inputs/out_MTR_2018.root_qcdDD.root ~/invisible/QCD/hig-20-003/MTR_2018/.
+cp -p inputs/out_VTR_2018.root_qcdDD.root ~/invisible/QCD/hig-20-003/VTR_2018/. 
+
+cp -p inputs/out_MTR_2017.root ~/invisible/QCD/hig-20-003/MTR_2017/.
+cp -p inputs/out_VTR_2017.root ~/invisible/QCD/hig-20-003/VTR_2017/. 
+cp -p inputs/out_MTR_2018.root ~/invisible/QCD/hig-20-003/MTR_2018/.
+cp -p inputs/out_VTR_2018.root ~/invisible/QCD/hig-20-003/VTR_2018/. 
+
+cp -p inputs/VTR_2017_noiseDD.root ~/invisible/QCD/hig-20-003/VTR_2017/.
+cp -p inputs/VTR_2018_noiseDD.root ~/invisible/QCD/hig-20-003/VTR_2018/.
+cp -p inputs/MTR_2017_noiseDD.root ~/invisible/QCD/hig-20-003/MTR_2017/.
+cp -p inputs/MTR_2018_noiseDD.root ~/invisible/QCD/hig-20-003/MTR_2018/.
+
+
